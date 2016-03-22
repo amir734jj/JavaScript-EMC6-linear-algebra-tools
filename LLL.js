8dabbfd286c4d981ddef267566c980b5050ea5cc
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2016-03-22 00:54:22 -0500

    Final working code

--- LLL.js
+++ LLL.js
@@ -5,110 +5,101 @@
 "use strict";
 
 var gs = require("./Gram-Schmidt Orthogonalization.js");
+var tools = require("./tools.js");
 
 class LLL {
-	constructor(matrix, delta) {
-		if (delta < 0.24 || delta > 1) {
-			throw "delta is out of range";
-		}
+    constructor(matrix, delta) {
+        if (delta < 0.25 || delta > 1) {
+            throw "delta is out of range";
+        }
 
-		this.matrix_before = matrix;
-		this.matrix_after = null;
-		this.delta = delta;
-		this.dimensions = matrix[0].length;
-		this.calculate_LLL();
-	}
+        this.matrix_before = matrix;
+        this.matrix_after = null;
+        this.matrix_before_before = null;
+        this.delta = delta;
+        this.dimensions = matrix[0].length;
+        this.calculate_LLL();
+    }
 
-	calculate_LLL() {
-		this.matrix_after = new gs(this.matrix_before, false).matrix;
-		var flag = false;
-		//console.log(this.matrix_after);
-		var k = 1;
-		do {
-			for (var j = k - 1; j >= 0; j--) {
-				console.log("before");
-				console.log(k + " " + j);
+    // ** important
+    //  {b} set of vectors are denoted by this.matrix_before
+    //  {b*} set of vectors are denoted by this.matrix_after
+    calculate_LLL() {
+        this.matrix_after = new gs(this.matrix_before, false).matrix; // initialize after vectors: perform Gram-Schmidt, but do not normalize
+        var flag = false; // invariant
+        var k = 1;
+        var loop_counter = 0;
 
-				if (this.mu(k, j) > 0.5) {
-					var to_subtract = this.multiply(Math.round(this.mu(k, j)), this.matrix_before[j]);
-					this.matrix_before[k] = this.subtract(this.matrix_before[k], to_subtract);
+        while (k < this.dimensions && !flag) { // I added this "flag" variable to prevent getting exceptions and terminate the loop gracefully
+            for (var j = k - 1; j >= 0; j--) {
+                if (Math.abs(this.mu(k, j)) > 0.5) {
+                    var to_subtract = tools.multiply(Math.round(this.mu(k, j)), this.matrix_before[j], this.dimensions);
+                    
+                    this.matrix_before_before = this.matrix_before; // hold previous value of {b} to prevent infinite loop
+                    this.matrix_before[k] = tools.subtract(this.matrix_before[k], to_subtract, this.dimensions);
 
-					this.matrix_after = new gs(this.matrix_before, false).matrix;
-				}
-			}
+                    this.matrix_after = new gs(this.matrix_before, false).matrix; // update after vectors: perform Gram-Schmidt, but do not normalize
+                }
+            }
 
-			if (this.dot_product(this.matrix_after[k], this.matrix_after[k]) > ((this.delta - (this.mu(k, k - 1) * this.mu(k, k - 1))) * (this.dot_product(this.matrix_after[k - 1], this.matrix_after[k - 1])))) {
-				if (k + 1 >= this.dimensions) { // invariant: there is some issue, something is wrong
-					flag = true;
-					console.log("something bad happened ! (1)");
-				}
-				k++;
-				console.log("if");
-				console.log(k + " " + j);
-			} else {
-				var temp_matrix = this.matrix_after[k];
-				this.matrix_after[k] = this.matrix_after[k - 1];
-				this.matrix_after[k - 1] = temp_matrix;
+            if (tools.dot_product(this.matrix_after[k], this.matrix_after[k], this.dimensions) >= (this.delta - Math.pow(this.mu(k, k - 1), 2)) * tools.dot_product(this.matrix_after[k - 1], this.matrix_after[k - 1], this.dimensions)) {
+                if (k + 1 > this.dimensions) { // invariant: there is some issue, something is wrong
+                    flag = true; // invariant is broken
+                    console.log("something bad happened ! (1)");
+                }
 
-				this.matrix_after = new gs(this.matrix_before, false).matrix;
+                k++;
+                // console.log("if; k, j");
+                // console.log(k + ", " + j);
+            } else {
+                this.matrix_before_before = this.matrix_before; // hold previous value of {b} to prevent infinite loop
+                var temp_matrix = this.matrix_before[k];
+                this.matrix_before[k] = this.matrix_before[k - 1];
+                this.matrix_before[k - 1] = temp_matrix;
 
-				if (k === Math.max(k - 1, 1) || k >= this.dimensions || Math.max(k - 1, 1) >= this.dimensions) { // invariant: there is some issue, something is wrong
-					flag = true;
-					console.log("something bad happened ! (2)");
-				}
-				k = Math.max(k - 1, 1);
+                this.matrix_after = new gs(this.matrix_before, false).matrix; // update after vectors: perform Gram-Schmidt, but do not normalize
 
+                if (k > this.dimensions || Math.max(k - 1, 1) > this.dimensions) { // invariant: there is some issue, something is wrong
+                    flag = true; // invariant is broken
+                    console.log("something bad happened ! (2)");
+                }
+                k = Math.max(k - 1, 1);
 
-				console.log("else");
-				console.log(k + " " + j);
-			}
-		}
-		while (k <= this.dimensions && !flag);
+                // console.log("else; k, j");
+                // console.log(k + ", " + j);
+            }
+            
+            // this will prevent infinite loop
+            if(loop_counter > Math.pow(this.dimensions, this.dimensions + 1) || loop_counter + 1 === Number.MAX_SAFE_INTEGER) {
+                flag = true; // prevent infinite loop, nothing new is happening
+            }
+            
+            // console.log(this.matrix_before);
+            
+            loop_counter++;
+        }
 
-		console.log("final: ");
-		console.log(this.matrix_before);
-	}
+        console.log("final: ");
+        console.log(this.matrix_before);
+    }
 
-	mu(i, j) {
-		var top = this.dot_product(this.matrix_before[i], this.matrix_after[j]);
-		var bottom = this.dot_product(this.matrix_after[j], this.matrix_after[j]);
+    // calculated mu as been  mentioned on Wikipedia
+    // mu(i, j) = <b_i, b*_j> / <b*_j, b*_j>
+    mu(i, j) {
+        var top = tools.dot_product(this.matrix_before[i], this.matrix_after[j], this.dimensions);
+        var bottom = tools.dot_product(this.matrix_after[j], this.matrix_after[j], this.dimensions);
 
-		return top / bottom;
-	}
-
-
-	subtract(vectorX, vectorY) {
-		var result = new Array(this.dimensions);
-		for (var i = 0; i < this.dimensions; i++) {
-			result[i] = vectorX[i] - vectorY[i];
-		}
-		return result;
-	}
-
-	multiply(scalarC, vectorX) {
-		var result = new Array(this.dimensions);
-		for (var i = 0; i < this.dimensions; i++) {
-			result[i] = scalarC * vectorX[i];
-		}
-		return result;
-	}
-
-	dot_product(vectorX, vectorY) {
-		var sum = 0;
-		for (var i = 0; i < this.dimensions; i++) {
-			sum += vectorX[i] * vectorY[i];
-		}
-		return sum;
-	}
+        return top / bottom;
+    }
 }
 
 
-/*
-	The following will run the LLL function
-*/
+
+// The following will run the LLL function
 var lll = new LLL(
-	[
-		[1, 0, 2],
-		[1, 2, 3],
-		[4, 5, 6]
-	], 3 / 4);
+    [
+        [1, 3, 1, 0],
+        [2, 2, 2, 6],
+        [3, 1, 3, 5],
+        [3, 1, 3, 5]
+    ], 1);
